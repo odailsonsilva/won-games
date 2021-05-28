c70e4d08bd57944c3fbc50ae53905a6ebe6feed4
odailson silva <odailsonsilva@MacBook-Air-de-odailson.local>
2021-05-28 17:44:17 -0300

    feat: config absolut src e theme p/ storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,13 @@
-import GlobalStyled from '../src/styles/global'
+import { ThemeProvider } from 'styled-components'
+
+import theme from 'styles/theme'
+import GlobalStyled from 'styles/global'
 
 export const decorators = [
   (Story) => (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyled />
       <Story />
-    </>
+    </ThemeProvider>
   )
 ]
