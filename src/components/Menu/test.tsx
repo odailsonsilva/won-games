682833af11236eec3fc6e99fe91fc5b8346f40e1
odailson silva <odailsonsilva@MacBook-Air-de-odailson.local>
2021-05-31 13:40:47 -0300

    feat: componente menu

---
+++ src/components/Menu/test.tsx
@@ -0,0 +1,55 @@
+import { fireEvent, screen } from '@testing-library/react'
+import { renderWithTheme } from '../../utils/tests/helpers'
+
+import { Menu } from '.'
+
+describe('<Menu />', () => {
+  it('should render the menu', () => {
+    renderWithTheme(<Menu />)
+
+    expect(screen.getByRole('img', { name: /won games/i })).toBeInTheDocument()
+    expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/search/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/Open Shopping Card/i)).toBeInTheDocument()
+  })
+
+  it('should handle the open/close mobile menu', () => {
+    renderWithTheme(<Menu />)
+
+    //select nav
+    const fullMenuElement = screen.getByRole('navigation', { hidden: true })
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
+    expect(fullMenuElement).toHaveStyle({ opacity: 0 })
+
+    //open
+    fireEvent.click(screen.getByLabelText(/open menu/i))
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('false')
+    expect(fullMenuElement).toHaveStyle({ opacity: 1 })
+
+    //close
+    fireEvent.click(screen.getByLabelText(/Close Menu/i))
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
+    expect(fullMenuElement).toHaveStyle({ opacity: 0 })
+  })
+
+  it('should show regiter box when logged out', () => {
+    renderWithTheme(<Menu />)
+    expect(screen.getByText(/log in now/i)).toBeInTheDocument()
+    expect(screen.getByText(/sign up/i)).toBeInTheDocument()
+  })
+
+  it('should show regiter box when logged out', () => {
+    renderWithTheme(<Menu />)
+    expect(screen.getByText(/Log in now/i)).toBeInTheDocument()
+    expect(screen.getByText(/Sign Up/i)).toBeInTheDocument()
+  })
+
+  it('should show wishlight and account when logged in', () => {
+    renderWithTheme(<Menu username="odailson" />)
+
+    expect(screen.getByText(/my account/i)).toBeInTheDocument()
+    expect(screen.getByText(/wishlist/i)).toBeInTheDocument()
+    expect(screen.queryByText(/Log in now/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/Sign Up/i)).not.toBeInTheDocument()
+  })
+})
